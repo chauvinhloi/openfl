02526f72d688810aa10652e63331ffdc5c8031b2
Igor Davidyuk <igor.davidyuk@intel.com>
2021-11-22 02:11:59 -0800

    aggregator and collaborator run under sgx but no connection

--- openfl-docker/build_with_sgx.sh
+++ openfl-docker/build_with_sgx.sh
@@ -7,23 +7,45 @@ docker build -t openfl_base:pre_sgx -f Dockerfile.base ..
 
 docker build -t openfl_gramine -f Dockerfile.gramine ..
 
-# docker build -t ${FEDERATION} -f Dockerfile.workspace \
-#     --build-arg BASE_IMAGE=openfl_gramine \
-#     --build-arg WORKSPACE_NAME=${FEDERATION} \
-#     ${FEDERATION} 
+# Federation should not use CUDA-related packages
+    --build-arg BASE_IMAGE=openfl_gramine \
+    --build-arg WORKSPACE_NAME=${FEDERATION} \
+    ${FEDERATION} 
 
-# # aggregator
-# docker run -it --rm --network=host --device=/dev/sgx_enclave --device=/dev/sgx_provision \
-#     --volume=/var/run/aesmd:/var/run/aesmd \
-#     --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/cert:/home/user/workspace/cert \
-#     --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/plan:/home/user/workspace/plan \
-#     --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/save:/home/user/workspace/save \
-#     ${FEDERATION} fx aggregator start
-
-# # collaborator
-
-
+# aggregator
+# make sure user can write to monted folders on host machine
+# INSECURE: key is generated to the manifest dir with "openssl genrsa -3 -out key.pem 3072"
+# Process is run from the manifest dir with "make SGX=1 SGX_SIGNER_KEY=key.pem"
+FEDERATION=fed_work12345alpha81671
 docker run -it --rm --network=host --device=/dev/sgx_enclave --device=/dev/sgx_provision \
     --volume=/var/run/aesmd:/var/run/aesmd \
-    --volume=/home/idavidyu/openfl/openfl-docker/sample_app:/sample_app \
-    openfl_gramine bash
+    --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/cert:/home/user/workspace/cert \
+    --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/plan:/home/user/workspace/plan \
+    --mount type=bind,src=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/save,dst=/home/user/workspace/save,readonly=0 \
+    --mount type=bind,src=/home/idavidyu/openfl/openfl-docker/aggregator_manifest,dst=/home/user/workspace/aggregator_manifest,readonly=0 \
+    ${FEDERATION} bash
+    # to run aggregator in container without gramine fx aggregator start
+    # to muild SGX app generate key: cd aggregator_manifest && openssl genrsa -3 -out key.pem 3072
+    # make clean && make SGX=1 SGX_SIGNER_KEY=key.pem 
+
+# collaborator
+COL_NAME=one123dragons
+COL_NAME=beta34unicorns
+FEDERATION=fed_work12345alpha81671
+docker run -it --rm --network=host --device=/dev/sgx_enclave --device=/dev/sgx_provision \
+    --volume=/var/run/aesmd:/var/run/aesmd \
+    --volume=/home/idavidyu/.openfl/data:/home/user/.openfl/data \
+    --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/${COL_NAME}/${FEDERATION}/cert:/home/user/workspace/cert \
+    --volume=/home/idavidyu/openfl/openfl-docker/${FEDERATION}/${COL_NAME}/${FEDERATION}/plan:/home/user/workspace/plan \
+    --mount type=bind,src=/home/idavidyu/openfl/openfl-docker/collaborator_manifest,dst=/home/user/workspace/collaborator_manifest,readonly=0 \
+    --env no_proxy=${no_proxy},$(hostname --all-fqdns | awk '{print $1}') \
+    --env COL_NAME=${COL_NAME} \
+    ${FEDERATION} bash
+
+# pip uninstall -y torch && pip install pip install torch==1.10.0+cpu torchvision==0.11.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
+
+# docker run -it --rm --network=host --device=/dev/sgx_enclave --device=/dev/sgx_provision \
+#     --volume=/var/run/aesmd:/var/run/aesmd \
+#     --volume=/home/idavidyu/openfl/openfl-docker/sample_app:/sample_app \
+#     openfl_gramine bash
